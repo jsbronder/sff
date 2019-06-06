b44dbd395b48c61f01a7771a751b6daaa9a12902
Justin Bronder <jsbronder@cold-front.org>
2019-06-05 21:44:01 -0400

    region: initial implementation
    Define a left-to-right, top-to-bottom section of text and common
    operations on it.

---
+++ qvterm/region.cpp
@@ -0,0 +1,57 @@
+#include "region.hpp"
+
+Region::Region(QPoint start, QPoint end) :
+    m_start(std::move(start)),
+    m_end(std::move(end))
+{
+}
+
+Region Region::fromPixelSpace(const QPoint &start, const QPoint &end, const QSize &cellSize)
+{
+    return {
+            {start.x() % cellSize.width(), start.y() / cellSize.height()},
+            {end.x() % cellSize.width(), end.y() / cellSize.height()}};
+}
+
+QString Region::dumpString(const QSize &termSize, const std::function<const VTermScreenCell *(int, int)> &fetchCell) const
+{
+    QString r{};
+    const VTermScreenCell *cell = nullptr;
+
+    for (int y = m_start.y(); y < m_end.y() + 1; ++y) {
+        int xStart = y == m_start.y() ? m_start.x() : 0;
+        int xEnd = y == m_end.y() ? m_end.x() + 1 : termSize.width();
+        int skipped = 0;
+
+        for (int x = xStart; x < xEnd; ++x) {
+            cell = fetchCell(x, y);
+            if (cell->chars[0]) {
+                for (; skipped; skipped--)
+                    r.append(' ');
+                r.append(QString::fromUcs4(cell->chars, cell->width));
+            } else {
+                skipped++;
+            }
+        }
+
+        if (!cell->chars[0])
+            r.append('\n');
+    }
+    return r;
+}
+
+QRect Region::pixelRect(const QSize &termSize, const QSize &cellSize) const
+{
+    bool oneLine = m_start.y() == m_end.y();
+    QPoint topLeft{
+            oneLine ? m_start.x() * cellSize.width() : 0,
+            m_start.y() * cellSize.height()};
+
+    // Essentially rounding up to make sure we create a bounding box that
+    // fully captures the text.
+    QPoint bottomRight{
+            (oneLine ? m_end.x() + 1 : termSize.width()) * cellSize.width(),
+            (1 + m_end.y()) * cellSize.height()};
+
+    return {topLeft, bottomRight};
+}
---
+++ qvterm/region.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include <QPoint>
+#include <QRect>
+#include <QSize>
+#include <QString>
+
+#include <functional>
+
+extern "C" {
+#include <vterm.h>
+}
+
+/**
+ * Define a left-to-right, top-to-bottom selection of characters in VTerm space.
+ **/
+class Region {
+public:
+    Region() = default;
+
+    /**
+     * Create a new region
+     *
+     * @param start - Start position in VTerm space
+     * @param end   - End position in VTerm space
+     **/
+    Region(QPoint start, QPoint end);
+
+    /**
+     * Create a new region
+     *
+     * @param start     - Start position in pixel space
+     * @param end       - End position in pixel space
+     * @param cellSize  - Size of a VTerm cell
+     *
+     * @return  - New region
+     **/
+    static Region fromPixelSpace(const QPoint &start, const QPoint &end, const QSize &cellSize);
+
+    /**
+     * Test if a point is inside of the region
+     *
+     * @param x - x coordinate
+     * @param y - y coordinate
+     *
+     * @return  - true if the point is in the region, false otherwise
+     **/
+    bool contains(int x, int y) const
+    {
+        return (!isNull()
+                && (y > m_start.y() || (y == m_start.y() && x >= m_start.x()))
+                && (y < m_end.y() || (y == m_end.y() && x <= m_end.x())));
+    }
+
+    /**
+     * Dump the contents of the region.  Empty cells will be ignored unless they
+     * are followed on the same line by non-empty cells.  That is, this performs
+     * the expected behavior for a copy operation on a selected region.
+     *
+     * @param termSize  - size of terminal
+     * @param fetchCell - function that will return VTermScreenCells for a given
+     *                    position
+     **/
+    QString dumpString(const QSize &termSize, const std::function<const VTermScreenCell *(int, int)> &fetchCell) const;
+
+    /**
+     * Test if the region is Null
+     **/
+    bool isNull() const
+    {
+        return m_start.isNull() && m_end.isNull();
+    }
+
+    /**
+     * Translate region into a rectangle in pixel space
+     *
+     * @param termSize  - size of terminal
+     * @param cellSize  - size of a VTerm cell
+     **/
+    QRect pixelRect(const QSize &termSize, const QSize &cellSize) const;
+
+    /**
+     * Update the region
+     *
+     * @param start - New start point
+     * @param end   - New end point
+     **/
+    void update(QPoint start, QPoint end)
+    {
+        m_start = std::move(start);
+        m_end = std::move(end);
+    }
+
+private:
+    QPoint m_start;
+    QPoint m_end;
+};
