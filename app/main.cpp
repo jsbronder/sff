c566511ed5e13605054cee3644351621639d94bc
Justin Bronder <jsbronder@cold-front.org>
2019-08-09 17:17:33 -0400

    app: add url match and copying to clipboard

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,22 @@
 #include <QApplication>
+#include <QClipboard>
+#include <QDebug>
 #include <QMainWindow>
+#include <QRegularExpression>
 #include <QShortcut>
 
 #include <qvterm.hpp>
+
+// clang-format off
+constexpr const char *urlMatch =
+    R"((?:https?://|ftp://|news://|mailto:|file://|\bwww\.))"
+    R"([\w\-\@;\/?:&=%\$.+!*\x27,~#]*)"
+    "("
+        R"(\([\w\-\@;\/?:&=%\$.+!*\x27,~#]*\))"
+        "|"
+        R"([\w\-\@;\/?:&=%\$+*~])"
+    ")+";
+// clang-format on
 
 int main(int argc, char **argv)
 {
@@ -34,6 +48,28 @@ int main(int argc, char **argv)
         qvterm.scrollPage(-1);
     });
 
+    bool matching = false;
+    QShortcut esc{QKeySequence{Qt::Key_Escape}, &win};
+    win.connect(&esc, &QShortcut::activated, [&qvterm, &esc, &matching]() {
+        qvterm.matchClear();
+        esc.setEnabled(false);
+        matching = false;
+    });
+    esc.setEnabled(false);
+
+    QShortcut findUrl{QKeySequence{Qt::CTRL + Qt::Key_M}, &win};
+    win.connect(&findUrl, &QShortcut::activated, [&qvterm, &matching, &esc]() {
+        static QRegularExpression re(urlMatch,
+                QRegularExpression::DotMatchesEverythingOption | QRegularExpression::CaseInsensitiveOption);
+
+        esc.setEnabled(true);
+        if (matching) {
+            qvterm.matchNext();
+        } else {
+            matching = true;
+            qvterm.match(&re);
+        }
+    });
+
     app.exec();
 }
-
